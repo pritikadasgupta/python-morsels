8c8cd466a2f9c0651a1cc2d30cf7e48a5e9f0f9c
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2021-01-04 19:47:11 -0500

    successful bonus1 soln

--- circle.py
+++ circle.py
@@ -5,7 +5,11 @@ import math
 class Circle():
 	def __init__(self, radius=1):
 		self.radius = radius
-		self.diameter = radius*2
-		self.area = (math.pi)*(radius**2)
 	def __repr__(self):
 		return("Circle({:.0f})".format(self.radius))
+	@property
+	def diameter(self):
+		return(float("{}".format((self.radius*2))))
+	@property
+	def area(self):
+		return(float("{}".format((math.pi)*(self.radius**2))))
