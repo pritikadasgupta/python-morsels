b45fb954df0a3f35f4cdce9d64cffe09c8d24c94
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2021-01-04 20:29:30 -0500

    successful bonus2 and bonus3 solns

--- circle.py
+++ circle.py
@@ -3,13 +3,33 @@
 import math
 
 class Circle():
-	def __init__(self, radius=1):
+	def __init__(self,radius=1):
 		self.radius = radius
+
 	def __repr__(self):
 		return("Circle({:.0f})".format(self.radius))
-	@property
-	def diameter(self):
-		return(float("{}".format((self.radius*2))))
-	@property
-	def area(self):
-		return(float("{}".format((math.pi)*(self.radius**2))))
+
+	def new_radius(self,name):
+		if name < 0:
+			raise ValueError("Radius cannot be negative")
+		self.radius1 = name
+
+	def get_new_radius(self):
+		return(self.radius1)
+
+	def new_diameter(self,name):
+		self.radius = name/2
+
+	def get_new_diameter(self):
+		return(self.radius*2)
+
+	def new_area(self,name):
+		raise AttributeError("can\'t set attribute")
+
+	def get_new_area(self):
+		return(math.pi*self.radius**2)
+
+	radius = property(get_new_radius,new_radius)
+	diameter = property(get_new_diameter,new_diameter)
+	area = property(get_new_area,new_area)
+	
