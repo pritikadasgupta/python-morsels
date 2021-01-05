ade20be82e002989a3b0ba2be74c233d2601d4b5
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2021-01-04 21:07:56 -0500

    add bonus1 and bonus2 solns

--- add.py
+++ add.py
@@ -1,4 +1,13 @@
 #!/bin/python3
 
-def add(*mats):
-	return([[sum(values) for values in zip(*rows)]for rows in zip(*mats)])
+from itertools import zip_longest
+
+def add(*matrices):
+	"""Add corresponding numbers in given 2-D matrices."""
+	try:
+		return [
+			[sum(values) for values in zip_longest(*rows)]
+			for rows in zip_longest(*matrices)
+		]
+	except TypeError as e:
+		raise ValueError("Given matrices are not the same size.") from e
