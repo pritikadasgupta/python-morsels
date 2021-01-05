69f150d6a544985b0971904f5f4bcfc023260598
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2021-01-04 21:06:16 -0500

    add bonus1 soln one line

--- add.py
+++ add.py
@@ -1,10 +1,4 @@
 #!/bin/python3
 
 def add(*mats):
-	mat3 = []
-	for rows in zip(*mats):
-		row=[]
-		for values in zip(*rows):
-			row.append(sum(values))
-		mat3.append(row)
-	return(mat3)
+	return([[sum(values) for values in zip(*rows)]for rows in zip(*mats)])
