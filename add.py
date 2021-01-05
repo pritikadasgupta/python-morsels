e66676176d0e04a0eb94560c382e7bfedd7a0095
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2021-01-04 21:05:16 -0500

    add bonus1 soln

--- add.py
+++ add.py
@@ -5,9 +5,6 @@ def add(*mats):
 	for rows in zip(*mats):
 		row=[]
 		for values in zip(*rows):
-			total = 0
-			for n in values:
-				total += n
-			row.append(total)
+			row.append(sum(values))
 		mat3.append(row)
 	return(mat3)
