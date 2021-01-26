c3dc8ac7ea0f73ea80c90c1d4083ac5e920cce17
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2021-01-26 01:43:44 -0500

    soln

--- parse_ranges.py
+++ parse_ranges.py
@@ -3,7 +3,11 @@
 def parse_ranges(string):
 	string1 = string.split(",")
 	for range_num in string1:
-		first_num, second_num = [int(x) for x in range_num.split('-')]
-		for num in range(first_num,second_num+1):
-			yield num
+		range_num = range_num.split('->')[0]
+		try:
+			yield int(range_num)
+		except ValueError:
+			first_num, second_num = [int(x) for x in range_num.split('-')]
+			for num in range(first_num,second_num+1):
+				yield num
 
